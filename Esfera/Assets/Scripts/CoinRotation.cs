747d47d6007bbfc6d2a077ec1fab4b334ec5891a
DanaroNait2002 <danaronait2002@gmail.com>
2022-10-07 19:55:28 +0100

    Pocos cambios
    Intenté hacer cosas, pero estoy muy cansada :'(

---
+++ Esfera/Assets/Scripts/CoinRotation.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinRotation : MonoBehaviour
+{
+    //Rotacion de las monedas
+    [SerializeField]
+    float rotationX = 50f;
+
+    //Funcion de rotacion de las monedas
+    void Update()
+    {
+        transform.Rotate(rotationX * Time.deltaTime, 0.0f, 0.0f);
+    }
+}
--- Esfera/Assets/Scripts/PlayerController.cs
+++ Esfera/Assets/Scripts/PlayerController.cs
@@ -1,24 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
     //Variables de Movimiento
-    public float movementX;
-    public float movementY;
-    public float movementZ;
+    //public float movementX;
+    //public float movementY;
+    //public float movementZ;
+    //Rigidbody body;
+    Vector3 direction;
+
+    //Constante de impulso
+    [SerializeField]
+    float impulse = 5f;
+
 
     //Constante de Velocidad
     [SerializeField]
-    float velocidadMovimiento = 1.5f;
+    //float velocidadMovimiento = 1.5f;
 
     void Update()
     {
         //Valor de Movimiento en el Mando/Teclado
-        movementX = Input.GetAxis("Horizontal") * Time.deltaTime * velocidadMovimiento;
-        movementZ = Input.GetAxis("Vertical") * Time.deltaTime * velocidadMovimiento;
-        transform.Translate(movementX, movementY, movementZ);
+        //movementX = Input.GetAxis("Horizontal") * Time.deltaTime * velocidadMovimiento;
+        //movementZ = Input.GetAxis("Vertical") * Time.deltaTime * velocidadMovimiento;
+        //transform.Translate(movementX, movementY, movementZ);
+        //Obtengo datos de movimiento en el mando/teclado
+        direction.x = Input.GetAxis("Horizontal") * Time.deltaTime * impulse;
+        direction.z = Input.GetAxis("Vertical") * Time.deltaTime * impulse;
+        transform.Translate(direction);
+    }
+
+    private void FixedUpdate()
+    {
+        //Empujo hacia donde se mueve
+        //body.AddForce(direction, ForceMode.Impulse);
     }
 }
